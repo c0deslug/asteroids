c5e11728aaf9b05a551cfee2053c1e4f2a5c77c9
c0deslug <s.rados37@gmail.com>
2025-05-05 19:57:57 +0200

    added black window, gitignore

--- main.py
+++ main.py
@@ -2,11 +2,23 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        screen.fill((255, 165, 0), rect=None, special_flags=0) # give 255, 165, 0 for orange - https://www.pygame.org/docs/ref/color.html
+        pygame.display.flip()
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 
 
