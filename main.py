80060d1941c941830307377f1ff2df7694b913c0
c0deslug <s.rados37@gmail.com>
2025-05-06 20:03:16 +0200

    added fps counter in console and dv var

--- main.py
+++ main.py
@@ -11,14 +11,25 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    AstroClock = pygame.time.Clock()
+    dt = 0 
     
     while True:
-        screen.fill((255, 165, 0), rect=None, special_flags=0) # give 255, 165, 0 for orange - https://www.pygame.org/docs/ref/color.html
-        pygame.display.flip()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        screen.fill((0, 0, 0), rect=None, special_flags=0) # give 255, 165, 0 for orange - https://www.pygame.org/docs/ref/color.html
+        pygame.display.flip()
+
+        dt = (AstroClock.tick(60) / 1000)
+
+        #print(dt)
+        #print(AstroClock.get_fps())
+
+
+            
+        
 
 
 
